911c5cad5a2bd8b9a43c480938329e95a8e70524
FrenchMasterSword <frenchmastersword@gmail.com>
2018-04-30 14:42:11 +0200

    Weather trigger_typing + corrected errors message in run command

--- bot.py
+++ bot.py
@@ -39,33 +39,34 @@ async def ping(ctx):
 @bot.command()
 async def weather(ctx, location):
     """Gives you current weather stuff on a given city"""
-    async with aiohttp.ClientSession() as client_session:
-        async with client_session.get(urllib.parse.quote_plus(weather_url.format(location), safe=';/?:@&=$,><-[]')) as response:
-            if response.status == 200:
-                data = await response.json()
+    async with ctx.typing():
+        async with aiohttp.ClientSession() as client_session:
+            async with client_session.get(urllib.parse.quote_plus(weather_url.format(location), safe=';/?:@&=$,><-[]')) as response:
+                if response.status == 200:
+                    data = await response.json()
 
-                temperature = data['main']['temp']
-                windSpeed = data['wind']['speed']
-                sunrise = str(datetime.utcfromtimestamp(data['sys']['sunrise'])).split(" ")[1]
-                sunset = str(datetime.utcfromtimestamp(data['sys']['sunset'])).split(" ")[1]
-                humidity = data['main']['humidity']
-                pressure = data['main']['pressure']
-                city = data['name'].capitalize()
-                countryFlag = f':flag_{data["sys"]["country"].lower()}:'
+                    temperature = data['main']['temp']
+                    windSpeed = data['wind']['speed']
+                    sunrise = str(datetime.utcfromtimestamp(data['sys']['sunrise'])).split(" ")[1]
+                    sunset = str(datetime.utcfromtimestamp(data['sys']['sunset'])).split(" ")[1]
+                    humidity = data['main']['humidity']
+                    pressure = data['main']['pressure']
+                    city = data['name'].capitalize()
+                    countryFlag = f':flag_{data["sys"]["country"].lower()}:'
 
-                emb = discord.Embed(title=f"Current weather at {city} {countryFlag} : {data['weather'][0]['description']}", color=BLUE)
-                emb.set_thumbnail(url=earth_url)
-                emb.add_field(name=":thermometer: Temperature",value=f"{temperature} °C")
-                emb.add_field(name=":dash: Wind speed",value=f"{windSpeed} m/s")
-                emb.add_field(name=":sunrise_over_mountains: Sunrise",value=f'{sunrise} (UTC)')
-                emb.add_field(name=":city_sunset: Sunset",value=f'{sunset} (UTC)')
-                emb.add_field(name=":droplet: Humidity",value=f'{humidity} %')
-                emb.add_field(name=":cloud: Pressure",value=f'{pressure} hPa')
+                    emb = discord.Embed(title=f"Current weather at {city} {countryFlag} : {data['weather'][0]['description']}", color=BLUE)
+                    emb.set_thumbnail(url=earth_url)
+                    emb.add_field(name=":thermometer: Temperature",value=f"{temperature} °C")
+                    emb.add_field(name=":dash: Wind speed",value=f"{windSpeed} m/s")
+                    emb.add_field(name=":sunrise_over_mountains: Sunrise",value=f'{sunrise} (UTC)')
+                    emb.add_field(name=":city_sunset: Sunset",value=f'{sunset} (UTC)')
+                    emb.add_field(name=":droplet: Humidity",value=f'{humidity} %')
+                    emb.add_field(name=":cloud: Pressure",value=f'{pressure} hPa')
 
-                emb.set_footer(text="powered by openweathermap.org")
-                await ctx.send(embed=emb)
-            else:
-                await ctx.send("An error occurred. Check your arguments.")
+                    emb.set_footer(text="powered by openweathermap.org")
+                    await ctx.send(embed=emb)
+                else:
+                    await ctx.send(f"An error occurred, code : {response.status}. Check your arguments.")
 
 @bot.command(name="run", hidden=True)
 async def run_code(ctx, *, text: str):
@@ -107,12 +108,12 @@ async def run_code(ctx, *, text: str):
                     if donnees['Errors']:
                         # Red markdown
                         listErrors = donnees['Errors'].split('\n')
+                        for line in listErrors:
+                            listErrors[listErrors.index(line)] = '-' + line
                         if len(listErrors) > 1:
-                            for line in listErrors:
-                                listErrors[listErrors.index(line)] = '-' + line
+                            donnees['Errors'] = "diff\n" + "\n".join(listErrors)[:-1]
                         else:
-                            listErrors[0] = '-' + listErrors[0]
-                        donnees['Errors'] = "diff\n" + "\n".join(listErrors)[:-1]
+                            donnees['Errors'] = "diff\n" + "\n".join(listErrors)
 
                         content += f'```{donnees["Errors"]}```'
                     if donnees['Stats']:
