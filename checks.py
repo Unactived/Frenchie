a31e49a42bb795f480b450a0c7ebfbcd90b2c687
FrenchMasterSword <frenchmastersword@gmail.com>
2018-05-06 16:27:14 +0200

    added admin and mod checks

--- checks.py
+++ checks.py
@@ -1,7 +1,21 @@
 import discord
 from discord.ext import commands
 
+# FMS -> bot owner
+# admin -> manage guild permission
+# moderator -> manage messages permission
+
 def is_FMS():
-    async def predicate(ctx):
+    async def pred(ctx):
         return ctx.author.id == 305289547430756354
-    return commands.check(predicate)
+    return commands.check(pred)
+
+def is_admin():
+    async def pred(ctx):
+        return await check_guild_permissions(ctx, {'manage_guild': True})
+    return commands.check(pred)
+
+def is_mod():
+    async def pred(ctx):
+        return await check_guild_permissions(ctx, {'manage_messages': True})
+    return commands.check(pred)
