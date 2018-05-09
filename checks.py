d68b67ee3f3b28f9e0e7f0abfed31926e7877ad0
FrenchMasterSword <frenchmastersword@gmail.com>
2018-05-09 20:07:00 +0200

    Revert "Removed is_FMS (is_owner exists)"
    This reverts commit e4a66d88ad432e10aac975327375854c6694dbee.

--- checks.py
+++ checks.py
@@ -5,6 +5,11 @@ from discord.ext import commands
 # admin -> manage guild permission
 # moderator -> manage messages permission
 
+def is_FMS():
+    async def pred(ctx):
+        return ctx.author.id == 305289547430756354
+    return commands.check(pred)
+
 def is_admin():
     async def pred(ctx):
         return await check_guild_permissions(ctx, {'manage_guild': True})
