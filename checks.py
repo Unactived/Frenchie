ff00e95fc9aea4e9a917f5855822e4da0d837710
FrenchMasterSword <frenchmastersword@gmail.com>
2018-05-17 00:25:13 +0200

    check_permissions in checks

--- checks.py
+++ checks.py
@@ -5,6 +5,14 @@ from discord.ext import commands
 # admin -> manage guild permission
 # moderator -> manage messages permission
 
+async def check_permissions(ctx, perms, *, check=all):
+    is_owner = await ctx.bot.is_owner(ctx.author)
+    if is_owner:
+        return True
+
+    resolved = ctx.channel.permissions_for(ctx.author)
+    return check(getattr(resolved, name, None) == value for name, value in perms.items())
+
 def is_admin():
     async def pred(ctx):
         return await check_guild_permissions(ctx, {'manage_guild': True})
