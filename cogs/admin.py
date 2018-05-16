efabeb0612d2cbe764f375a0354f1d7405502222
FrenchMasterSword <frenchmastersword@gmail.com>
2018-05-17 00:27:39 +0200

    admin check

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,9 +1,14 @@
 import discord
 from discord.ext import commands
 
+from checks import *
+
 class Administration:
     def __init__(self, bot):
         self.bot = bot
+
+    async def __local_check(self, ctx):
+        return is_admin(ctx.author)
 
 def setup(bot):
     bot.add_cog(Administration(bot))
