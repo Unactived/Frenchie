aad7a461fabcc7cf29335a3e88867f65394aba79
FrenchMasterSword <frenchmastersword@gmail.com>
2018-05-21 11:03:30 +0200

    Moved cpp and run and according modules

--- cogs/general.py
+++ cogs/general.py
@@ -1,10 +1,7 @@
 #import asyncio
 import discord
 from discord.ext import commands
-import aiohttp, urllib.parse
-from datetime import datetime
-import os
-import stackexchange
+import urllib.parse
 
 from config import *
 from checks import *
@@ -37,60 +34,6 @@ it's French, and still in development)"""
 
         await ctx.send(f':ping_pong: **Pong !** Latency : `{latency} ms`')
 
-    @commands.command(name="run", usage="<language>|[\`\`\`][<language>\n]<code>][\`\`\`]")
-    async def run_code(self, ctx, *, text: str):
-        """Run code and feedback Output, warnings, errors and performance"""
-        arg = text.split('|')
-        language = arg[0].capitalize()
-        code = arg[1][3:-3]
-        #code markdown, C++ and C#/F#
-        if code.startswith(arg[0]+'\n'):
-            code = code[len(arg[0])+1:]
-        elif code.startswith("cpp\n"):
-            code = code[4:]
-        elif code[1:].startswith('sharp\n'):
-            code = code[7:]
-
-        rexUrlRequest = rex_url.format(rexLanguageDict[language], code)
-        #TODO: add optional compiler args input # INPUT is useless
-        #if len(arg) > 2:
-        #    if arg[2].startswith('INPUT='):
-        #        rexUrlRequest += '&Input=' + arg[2][6:]
-        #    if not arg[-1].startswith('INPUT='):
-        #        rexUrlRequest += '&CompilerArgs=' + arg[-1]
-        if ('&CompilerArgs' not in rexUrlRequest) and (language in rexCompilerDict):
-            rexUrlRequest += '&CompilerArgs=' + rexCompilerDict[language]
-        #removing bad URL characters
-        rexUrlRequest = urllib.parse.quote_plus(rexUrlRequest, safe=';/?:@&=$,><-[]')
-        async with ctx.typing():
-            async with aiohttp.ClientSession() as client_session:
-                async with client_session.get(rexUrlRequest) as response:
-                    if response.status == 200:
-                        donnees = await response.json()
-                        content = ""
-                        if donnees['Result']:
-                            content += f'```{donnees["Result"]}```'
-                        if donnees['Warnings']:
-                            content += f'```fix\n{donnees["Warnings"]}```'
-                        if donnees['Errors']:
-                            # Red markdown
-                            listErrors = donnees['Errors'].split('\n')
-                            for line in listErrors:
-                                listErrors[listErrors.index(line)] = '-' + line
-                            if len(listErrors) > 1:
-                                donnees['Errors'] = "diff\n" + "\n".join(listErrors)[:-1]
-                            else:
-                                donnees['Errors'] = "diff\n" + "\n".join(listErrors)
-
-                            content += f'```{donnees["Errors"]}```'
-                        if donnees['Stats']:
-                            content += f'```{donnees["Stats"]}```'
-                        if not content:
-                            content = "```No output```"
-                        await ctx.send(content)
-                    else:
-                        await ctx.send(f"An error occurred, code {response.status}\nCommand usage : `.run <language>|```<code>```[|INPUT=<input>][|<compiler args>]`")
-
     @commands.command(aliases=['source'])
     async def sourcecode(self, ctx):
         """Grants you access to a horrible code which strikes you blind"""
@@ -115,63 +58,6 @@ it's French, and still in development)"""
         if ctx.invoked_subcommand is None:
 
             await ctx.send(f'Usage : `{prefix}ask <site> "Arguments"`')
-
-    @commands.command()
-    async def cpp(self, ctx, *, query: str):
-        """Search something on cppreference"""
-
-        url = 'http://en.cppreference.com/w/cpp/index.php'
-        params = {
-            'title': 'Special:Search',
-            'search': query
-        }
-
-        async with ctx.session.get(url, params=params) as resp:
-            if resp.status != 200:
-                return await ctx.send(f'An error occurred (status code: {resp.status}). Retry later.')
-
-            if len(resp.history) > 0:
-                return await ctx.send(resp.url)
-
-            e = discord.Embed()
-            root = etree.fromstring(await resp.text(), etree.HTMLParser())
-
-            nodes = root.findall(".//div[@class='mw-search-result-heading']/a")
-
-            description = []
-            special_pages = []
-            for node in nodes:
-                href = node.attrib['href']
-                if not href.startswith('/w/cpp'):
-                    continue
-
-                if href.startswith(('/w/cpp/language', '/w/cpp/concept')):
-                    # special page
-                    special_pages.append(f'[{node.text}](http://en.cppreference.com{href})')
-                else:
-                    description.append(f'[`{node.text}`](http://en.cppreference.com{href})')
-
-            if len(special_pages) > 0:
-                e.add_field(name='Language Results', value='\n'.join(special_pages), inline=False)
-                if len(description):
-                    e.add_field(name='Library Results', value='\n'.join(description[:10]), inline=False)
-            else:
-                if not len(description):
-                    return await ctx.send('No results found.')
-
-                e.title = 'Search Results'
-                e.description = '\n'.join(description[:15])
-
-            await ctx.send(embed=e)
-
-    @ask.command()
-    async def StackOverflow(self, ctx, arg):
-        """Subcommand of group ask ; searches on StackOverflow"""
-
-        site = stackexchange.Site(eval(f"stackexchange.{ctx.invoked_with}"),
-        app_key=SE_KEY, impose_throttling=True)
-
-        qs = so.search(intitle=arg)
 
     @commands.command()
     async def lmgtfy(self, ctx, *, text: str):
--- cogs/internet.py
+++ cogs/internet.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import aiohttp, urllib.parse
+from datetime import datetime
 
 import stackexchange as se
 
@@ -8,6 +10,54 @@ from config import *
 class Internet:
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(hidden=True)
+    async def cpp(self, ctx, *, query: str):
+        """Search something on cppreference"""
+
+        url = 'http://en.cppreference.com/w/cpp/index.php'
+        params = {
+            'title': 'Special:Search',
+            'search': query
+        }
+
+        async with ctx.session.get(url, params=params) as resp:
+            if resp.status != 200:
+                return await ctx.send(f'An error occurred (status code: {resp.status}). Retry later.')
+
+            if len(resp.history) > 0:
+                return await ctx.send(resp.url)
+
+            e = discord.Embed()
+            root = etree.fromstring(await resp.text(), etree.HTMLParser())
+
+            nodes = root.findall(".//div[@class='mw-search-result-heading']/a")
+
+            description = []
+            special_pages = []
+            for node in nodes:
+                href = node.attrib['href']
+                if not href.startswith('/w/cpp'):
+                    continue
+
+                if href.startswith(('/w/cpp/language', '/w/cpp/concept')):
+                    # special page
+                    special_pages.append(f'[{node.text}](http://en.cppreference.com{href})')
+                else:
+                    description.append(f'[`{node.text}`](http://en.cppreference.com{href})')
+
+            if len(special_pages) > 0:
+                e.add_field(name='Language Results', value='\n'.join(special_pages), inline=False)
+                if len(description):
+                    e.add_field(name='Library Results', value='\n'.join(description[:10]), inline=False)
+            else:
+                if not len(description):
+                    return await ctx.send('No results found.')
+
+                e.title = 'Search Results'
+                e.description = '\n'.join(description[:15])
+
+            await ctx.send(embed=e)
 
     @commands.command(aliases=['so'])
     async def stackoverflow(self, ctx, *, text: str):
@@ -31,6 +81,60 @@ class Internet:
 
         else:
             await ctx.send("No results")
+
+    @commands.command(name="run", usage="<language>|[\`\`\`][<language>\n]<code>][\`\`\`]")
+    async def run_code(self, ctx, *, text: str):
+        """Run code and feedback Output, warnings, errors and performance"""
+        arg = text.split('|')
+        language = arg[0].capitalize()
+        code = arg[1][3:-3]
+        #code markdown, C++ and C#/F#
+        if code.startswith(arg[0]+'\n'):
+            code = code[len(arg[0])+1:]
+        elif code.startswith("cpp\n"):
+            code = code[4:]
+        elif code[1:].startswith('sharp\n'):
+            code = code[7:]
+
+        rexUrlRequest = rex_url.format(rexLanguageDict[language], code)
+        #TODO: add optional compiler args input # INPUT is useless
+        #if len(arg) > 2:
+        #    if arg[2].startswith('INPUT='):
+        #        rexUrlRequest += '&Input=' + arg[2][6:]
+        #    if not arg[-1].startswith('INPUT='):
+        #        rexUrlRequest += '&CompilerArgs=' + arg[-1]
+        if ('&CompilerArgs' not in rexUrlRequest) and (language in rexCompilerDict):
+            rexUrlRequest += '&CompilerArgs=' + rexCompilerDict[language]
+        #removing bad URL characters
+        rexUrlRequest = urllib.parse.quote_plus(rexUrlRequest, safe=';/?:@&=$,><-[]')
+        async with ctx.typing():
+            async with aiohttp.ClientSession() as client_session:
+                async with client_session.get(rexUrlRequest) as response:
+                    if response.status == 200:
+                        donnees = await response.json()
+                        content = ""
+                        if donnees['Result']:
+                            content += f'```{donnees["Result"]}```'
+                        if donnees['Warnings']:
+                            content += f'```fix\n{donnees["Warnings"]}```'
+                        if donnees['Errors']:
+                            # Red markdown
+                            listErrors = donnees['Errors'].split('\n')
+                            for line in listErrors:
+                                listErrors[listErrors.index(line)] = '-' + line
+                            if len(listErrors) > 1:
+                                donnees['Errors'] = "diff\n" + "\n".join(listErrors)[:-1]
+                            else:
+                                donnees['Errors'] = "diff\n" + "\n".join(listErrors)
+
+                            content += f'```{donnees["Errors"]}```'
+                        if donnees['Stats']:
+                            content += f'```{donnees["Stats"]}```'
+                        if not content:
+                            content = "```No output```"
+                        await ctx.send(content)
+                    else:
+                        await ctx.send(f"An error occurred, code {response.status}\nCommand usage : `.run <language>|```<code>```[|INPUT=<input>][|<compiler args>]`")
 
     @commands.command(usage="<location>,[<country code>]")
     async def weather(self, ctx, location):
