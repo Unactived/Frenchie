3d76cd2abd2f009b58308cfaae7e5916e6685d16
FrenchMasterSword <frenchmastersword@gmail.com>
2018-05-21 11:11:49 +0200

    typing for so

--- cogs/internet.py
+++ cogs/internet.py
@@ -67,21 +67,22 @@ class Internet:
         so.impose_throttling = True
         so.throttle_stop = False
 
-        qs = so.search(intitle=text)[:3]
-        if qs:
-            emb = discord.Embed(title=text)
-            emb.set_thumbnail(url='https://cdn.sstatic.net/Sites/stackoverflow/company/img/logos/so/so-icon.png?v=c78bd457575a')
-            emb.set_footer(text="Hover for vote stats")
+        async with ctx.typing():
+            qs = so.search(intitle=text)[:3]
+            if qs:
+                emb = discord.Embed(title=text)
+                emb.set_thumbnail(url='https://cdn.sstatic.net/Sites/stackoverflow/company/img/logos/so/so-icon.png?v=c78bd457575a')
+                emb.set_footer(text="Hover for vote stats")
 
-            for q in qs:
-                q = so.question(q.id, filter="!b1MME4lS1P-8fK") # Fetch question's data, include vote_counts and answers
-                emb.add_field(name=f"`{len(q.answers)} answers` Score : {q.score}",
-                value=f'[{q.title}](https://stackoverflow.com/q/{q.id} "{q.up_vote_count}🔺|{q.down_vote_count}🔻")',
-                inline=False)
-            await ctx.send(embed=emb)
+                for q in qs:
+                    q = so.question(q.id, filter="!b1MME4lS1P-8fK") # Fetch question's data, include vote_counts and answers
+                    emb.add_field(name=f"`{len(q.answers)} answers` Score : {q.score}",
+                    value=f'[{q.title}](https://stackoverflow.com/q/{q.id} "{q.up_vote_count}🔺|{q.down_vote_count}🔻")',
+                    inline=False)
+                await ctx.send(embed=emb)
 
-        else:
-            await ctx.send("No results")
+            else:
+                await ctx.send("No results")
 
     @commands.command(name="run", usage="<language>|[\`\`\`][<language>\n]<code>][\`\`\`]")
     async def run_code(self, ctx, *, text: str):
