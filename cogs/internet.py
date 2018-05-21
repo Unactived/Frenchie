d1e97d62f897bee9f98ac655d93a9e4191e68d83
FrenchMasterSword <frenchmastersword@gmail.com>
2018-05-21 10:53:24 +0200

    Moved weather to internet cog

--- cogs/internet.py
+++ cogs/internet.py
@@ -32,5 +32,37 @@ class Internet:
         else:
             await ctx.send("No results")
 
+    @commands.command(usage="<location>,[<country code>]")
+    async def weather(self, ctx, location):
+        """Gives you current weather stuff on a given city"""
+        async with ctx.typing():
+            async with aiohttp.ClientSession() as client_session:
+                async with client_session.get(urllib.parse.quote_plus(weather_url.format(location), safe=';/?:@&=$,><-[]')) as    response:
+                    if response.status == 200:
+                        data = await response.json()
+
+                        temperature = data['main']['temp']
+                        windSpeed = data['wind']['speed']
+                        sunrise = str(datetime.utcfromtimestamp(data['sys']['sunrise'])).split(" ")[1]
+                        sunset = str(datetime.utcfromtimestamp(data['sys']['sunset'])).split(" ")[1]
+                        humidity = data['main']['humidity']
+                        pressure = data['main']['pressure']
+                        city = data['name'].capitalize()
+                        countryFlag = f':flag_{data["sys"]["country"].lower()}:'
+
+                        emb = discord.Embed(title=f"Current weather at {city} {countryFlag} : {data['weather'][0]['description']}", color=BLUE)
+                        emb.set_thumbnail(url=earth_url)
+                        emb.add_field(name=":thermometer: Temperature",value=f"{temperature} °C")
+                        emb.add_field(name=":dash: Wind speed",value=f"{windSpeed} m/s")
+                        emb.add_field(name=":sunrise_over_mountains: Sunrise",value=f'{sunrise} (UTC)')
+                        emb.add_field(name=":city_sunset: Sunset",value=f'{sunset} (UTC)')
+                        emb.add_field(name=":droplet: Humidity",value=f'{humidity} %')
+                        emb.add_field(name=":cloud: Pressure",value=f'{pressure} hPa')
+
+                        emb.set_footer(text="powered by openweathermap.org")
+                        await ctx.send(embed=emb)
+                    else:
+                        await ctx.send(f"An error occurred, code : {response.status}. Check your arguments.")
+
 def setup(bot):
     bot.add_cog(Internet(bot))
